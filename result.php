ea39c27dc0cc30c74fcdad394dd9bd6bbc075586
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-25 15:44:16 +0200

    feat: add html and php code, end

--- result.php
+++ result.php
@@ -1,6 +1,14 @@
 <!-- PHP  -->
 <?php
 
+    $first_text = $_GET["first-text"];
+
+    $result = strlen($first_text);
+
+    $censured_word = $_GET["censured-word"];
+
+    $changed_text = str_replace($censured_word, '***', $first_text);
+    $new_result = strlen($changed_text);
 ?>
 
 
@@ -11,6 +19,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    
+    <p><?php var_dump($first_text)?></p>
+    <span><?php echo $result ?></span>
+<hr>
+<p><?php var_dump($changed_text)?></p>
+    <p><?php echo $changed_text ?></p>
+    <span><?php echo $new_result ?></span>
 </body>
 </html>
